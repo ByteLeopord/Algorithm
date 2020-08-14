108ac4fcdc894a7438431fae680d66d91f555e5c
ByteLeopord <68319185+ByteLeopord@users.noreply.github.com>
2020-08-14 22:58:57 +0800

    Create 最短路径(弗洛伊德).cpp

---
+++ 最短路径(弗洛伊德).cpp
@@ -0,0 +1,22 @@
+void Floyd(vector<vector<int>>& graph, vector<vector<int>>& dist, vector<vector<int>>& sequ){
+	int len = graph.size();
+	
+	for(int i = 0; i < len; ++i){
+		for(int j = 0; j < len; ++j){
+			dist[i][j] = graph[i][j];
+			sequ[i][j] = j;
+		}
+	}
+	
+	for(int k = 0; k < len; ++k){
+		for(int i = 0; i < len; ++i){
+			for(int j = 0; j < len; ++j){
+				if(dist[i][k] == INT_MAX || dist[k][j] == INT_MAX) continue;
+				if(dist[i][j] > dist[i][k] + dist[k][j]){
+					dist[i][j] = dist[i][k] + dist[k][j];
+					sequ[i][j] = sequ[i][k];
+				}
+			}
+		}
+	}
+}
