43597b5e562513d07e456e3ec437083289c19867
ByteLeopord <68319185+ByteLeopord@users.noreply.github.com>
2020-08-26 23:10:21 +0800

    Update 01背包问题.cpp

--- 01背包问题.cpp
+++ 01背包问题.cpp
@@ -1,6 +1,8 @@
 /*
 game_0记录物品的价值、game_1记录物品的重量、x记录背包容量
 dp[len+1][x+1]记录len种物品时、背包装入x重量时 最大价值
+dp[i][j] = max(dp[i-1][j], v[i-1] + dp[i-1][j-w[i-1]])
+解释：不放第i件物品 和 放第i件物品 取较大值
 */
 int dfs(vector<vector<int>>& game, int x){
     int len = game.size();
@@ -17,7 +19,7 @@ int dfs(vector<vector<int>>& game, int x){
         }
     }
     return dp[len][x];
-    //节约空间
+    //节约空间 一维数组做法
     //for(int i = 0; i < len; i++){
     //    int w = game[i][1];
     //    int v = game[i][0];
