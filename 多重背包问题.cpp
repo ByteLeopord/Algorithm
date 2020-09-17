a04e8477bc52c41aa73f6cc93d058c65a7d07668
ByteLeopord <68319185+ByteLeopord@users.noreply.github.com>
2020-09-17 20:22:32 +0800

    Update 多重背包问题.cpp

--- 多重背包问题.cpp
+++ 多重背包问题.cpp
@@ -2,7 +2,7 @@
 多重背包问题限定了一种物品的个数，解决多重背包问题，只需要把它转化为0-1背包问题即可。
 比如，有2件价值为5，重量为2的同一物品，我们就可以分为物品a和物品b，a和b的价值都为5，重量都为2，但我们把它们视作不同的物品。
 */
-int beibao(vector<int>& value, vector<int>& weight, int limit)
+int beibao(vector<int>& value, vector<int>& weight, vector<int>& num, int limit)
 {
 	int len = value.size();
 
